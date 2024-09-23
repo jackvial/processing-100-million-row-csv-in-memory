dbafe8180da254664a96e666e476684100b313f5
Jack Vial <vialjack@gmail.com>
2024-09-22 20:17:32 -0400

    update array of objects strings example

--- src/examples/arrayOfObjectsStringDictionary.ts
+++ src/examples/arrayOfObjectsStringDictionary.ts
@@ -2,41 +2,50 @@ import {
     getMemoryStats,
     MemoryStatsRow,
     writeStatsToCsv
-} from "../utils"
+} from "../utils";
 import csvParser from 'csv-parser';
 import fs from 'fs';
 
 export async function main() {
     const startTime = Date.now();
     const memoryStats: MemoryStatsRow[] = [];
-    const nRows = 10_000_000;
+    const nRows = 100_000_000;
 
-    // Color map to store smaller integers instead of full color names
-    const colorMap = {
-        0: 'red',
-        1: 'green',
-        2: 'blue',
-        3: 'purple'
-    };
+    // Create maps for chemicals and shippers
+    const chemMap: { [key: number]: string } = {};
+    const chemStringToIndex: { [key: string]: number } = {};
+    const shipperMap: { [key: number]: string } = {};
+    const shipperStringToIndex: { [key: string]: number } = {};
 
-    // A reverse lookup for color string to integer mapping
-    const colorStringToIndex = {
-        'red': 0,
-        'green': 1,
-        'blue': 2,
-        'purple': 3
-    };
+    let chemIndex = 0;
+    let shipperIndex = 0;
 
     const rows: any[] = [];
-    const filePath = 'outputs/test_10000000_rows.csv';
+    const filePath = 'outputs/chemicals_shipped_100000000.csv';
     const readStream = fs.createReadStream(filePath);
 
     let rowIndex = 0;
     await new Promise<void>((resolve, reject) => readStream.pipe(csvParser()).on('data', (row: any) => {
-        const colorIndex = colorStringToIndex[row.color];
+        if (!(row.chem_name in chemStringToIndex)) {
+            chemStringToIndex[row.chem_name] = chemIndex;
+            chemMap[chemIndex] = row.chem_name;
+            chemIndex++;
+        }
+
+        if (!(row.shipper in shipperStringToIndex)) {
+            shipperStringToIndex[row.shipper] = shipperIndex;
+            shipperMap[shipperIndex] = row.shipper;
+            shipperIndex++;
+        }
+
+        const chemIndexMapped = chemStringToIndex[row.chem_name];
+        const shipperIndexMapped = shipperStringToIndex[row.shipper];
+
         rows.push({
-            price: parseFloat(row.price),
-            colorIndex
+            chemIndex: chemIndexMapped,
+            amount: parseFloat(row.amount),
+            shipperIndex: shipperIndexMapped,
+            shippedAt: parseInt(row.shipped_at, 10)
         });
 
         if (rowIndex % 1_000_000 === 0) {
